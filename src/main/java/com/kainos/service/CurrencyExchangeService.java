af6eaad1025ff5b10de257679ad266255aba72b4
KonradSto <stolarczyk.konrad@outlook.com>
2019-05-26 20:05:01 +0200

    adds service test

--- src/main/java/com/kainos/service/CurrencyExchangeService.java
+++ src/main/java/com/kainos/service/CurrencyExchangeService.java
@@ -1,7 +1,29 @@
 package com.kainos.service;
 
+import java.io.IOException;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kainos.model.RealtimeCurrencyTrade;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurrencyExchangeService {
+
+    private RealtimeCurrencyTrade realtimeCurrencyTrade;
+
+    public CurrencyExchangeService(RealtimeCurrencyTrade realtimeCurrencyTrade) {
+        this.realtimeCurrencyTrade = realtimeCurrencyTrade;
+    }
+
+    public String getCurrentExchangeRate(String fromCurrency, String toCurrency) throws IOException {
+        if (fromCurrency == null || toCurrency == null) {
+            throw new IllegalArgumentException("Both from and to currency cannot be null");
+        }
+        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        realtimeCurrencyTrade = objectMapper.readValue(new URL("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency="
+            + fromCurrency + "&to_currency=" + toCurrency + "&apikey=IN52AZAATJWV4OQR"), RealtimeCurrencyTrade.class);
+        return realtimeCurrencyTrade.getRealtimeCurrencyTradeAttributes().getExchangeRate();
+    }
 }
---
+++ src/test/java/com/kainos/service/CurrencyExchangeServiceTest.java
@@ -0,0 +1,47 @@
+package com.kainos.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import com.kainos.model.RealtimeCurrencyTrade;
+import com.kainos.model.RealtimeCurrencyTradeAttributes;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
+class CurrencyExchangeServiceTest {
+
+    @InjectMocks
+    private CurrencyExchangeService currencyExchangeService;
+
+    @Mock
+    RealtimeCurrencyTrade realtimeCurrencyTrade;
+
+    private String fromCurrency = "USD";
+    private String toCurrency = "EUR";
+
+    @Test
+    void shouldReturnCurrentExchangeRate() throws IOException {
+        //Given
+        String expectedExchangeRate = "1.5";
+        RealtimeCurrencyTradeAttributes realtimeCurrencyTradeAttributes = mock(RealtimeCurrencyTradeAttributes.class);
+        when(realtimeCurrencyTradeAttributes.getExchangeRate()).thenReturn(expectedExchangeRate);
+        when(realtimeCurrencyTrade.getRealtimeCurrencyTradeAttributes()).thenReturn(realtimeCurrencyTradeAttributes);
+
+        //When
+        String currencyExchangeResult = currencyExchangeService.getCurrentExchangeRate(fromCurrency, toCurrency);
+
+        //Then
+        assertEquals(expectedExchangeRate, currencyExchangeResult);
+        verify(realtimeCurrencyTrade.getRealtimeCurrencyTradeAttributes()).getExchangeRate();
+    }
+}
+
