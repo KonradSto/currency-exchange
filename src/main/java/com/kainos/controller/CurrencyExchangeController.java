08dd856e454ce7307c9b6b26453c21d368d32630
KonradSto <stolarczyk.konrad@outlook.com>
2019-05-26 20:08:07 +0200

    controller class setup

--- src/main/java/com/kainos/controller/CurrencyExchangeController.java
+++ src/main/java/com/kainos/controller/CurrencyExchangeController.java
@@ -1,7 +1,45 @@
 package com.kainos.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.kainos.service.CurrencyExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CurrencyExchangeController {
+
+    private CurrencyExchangeService currencyExchangeService;
+
+    @Autowired
+    public CurrencyExchangeController(CurrencyExchangeService currencyExchangeService) {
+        this.currencyExchangeService = currencyExchangeService;
+    }
+
+    @GetMapping({"/"})
+    public ModelAndView showCurrencyExchangePanel(){
+        ModelAndView model = new ModelAndView("home");
+        String currencyCode = "";
+        List<String> currencyList = currencyExchangeService.setCurrencyCodesList();
+        model.addObject("currencyCode", currencyCode);
+        model.addObject("currencyList", currencyList);
+        return model;
+    }
+
+    @PostMapping("/exchange")
+    @ResponseBody
+    public ModelAndView exchangeCurrency(@RequestParam("From") String fromCurrency, @RequestParam("To") String toCurrency) throws IOException {
+        ModelAndView modelAndView = new ModelAndView("exchange");
+        modelAndView.addObject("From", fromCurrency);
+        modelAndView.addObject("To", toCurrency);
+        modelAndView.addObject("value", currencyExchangeService.getCurrentExchangeRate(fromCurrency, toCurrency));
+        modelAndView.addObject("currencyMap", currencyExchangeService.getHistoricalCurrencyExchangeMap(fromCurrency, toCurrency));
+        return modelAndView;
+    }
 }
