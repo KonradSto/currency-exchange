910c06e60a62ab7645b7896007be3cf5a8841317
KonradSto <stolarczyk.konrad@outlook.com>
2019-05-26 20:09:27 +0200

    missing beans added to configuration class

--- src/main/java/com/kainos/configuration/ApplicationConfiguration.java
+++ src/main/java/com/kainos/configuration/ApplicationConfiguration.java
@@ -1,7 +1,22 @@
 package com.kainos.configuration;
 
+import com.kainos.model.HistoricalCurrencyTrade;
+import com.kainos.model.RealtimeCurrencyTrade;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public RealtimeCurrencyTrade getRealtimeCurrencyTrade(){
+        RealtimeCurrencyTrade realtimeCurrencyTrade = new RealtimeCurrencyTrade();
+        return realtimeCurrencyTrade;
+    }
+
+    @Bean
+    public HistoricalCurrencyTrade getHistoricalCurrencyTrade(){
+        HistoricalCurrencyTrade historicalCurrencyTrade = new HistoricalCurrencyTrade();
+        return historicalCurrencyTrade;
+    }
 }
